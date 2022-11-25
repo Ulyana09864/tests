f046af908c9c468a2fe389cc7396c381ea7db04e
Ulyana09864 <113189344+Ulyana09864@users.noreply.github.com>
2022-11-25 17:45:17 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -53,7 +53,7 @@ next_question.addEventListener("click", f_next_question);
   }
 
 function answers(k){
-  query ='https://innovations.kh.ua/quiz/list/?author_id=72&n='+k;
+  query ='https://innovations.kh.ua/quiz/list/?author_id=73&n='+k;
   console.log(query);
   fetch(query).then(response => response.json())
   .then(function (quiz) {
